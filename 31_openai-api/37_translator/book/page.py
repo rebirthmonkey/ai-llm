437ffd41e333fca3e47ed8d96a54991f87cd8719
ruanhe <ruanhe@global.tencent.com>
2025-09-25 16:52:41 +0800

    init fine-tuning

--- 31_openai-api/37_translator/book/page.py
+++
@@ -1,8 +0,0 @@
-from .content import Content
-
-class Page:
-    def __init__(self):
-        self.contents = []
-
-    def add_content(self, content: Content):
-        self.contents.append(content)
